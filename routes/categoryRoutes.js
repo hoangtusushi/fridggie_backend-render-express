bd47225d48496ef5866f31b0cc3e5c8f270beccc
Hiepdv2003 <hiepdvgcs210944@fpt.edu.vn>
2024-09-18 16:31:19 +0700

    update route

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,10 +4,16 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', authenticate, categoryController.createCategory);
-router.get('/', authenticate, categoryController.getAllCategories);
-router.get('/:id', authenticate, categoryController.getCategoryById);
-router.put('/:id', authenticate, categoryController.updateCategory);
-router.delete('/:id', authenticate, categoryController.deleteCategory);
+router.post('/', categoryController.createCategory);
+router.get('/', categoryController.getAllCategories);
+router.get('/:id', categoryController.getCategoryById);
+router.put('/:id', categoryController.updateCategory);
+router.delete('/:id', categoryController.deleteCategory);
+
+// router.post('/', authenticate, categoryController.createCategory);
+// router.get('/', authenticate, categoryController.getAllCategories);
+// router.get('/:id', authenticate, categoryController.getCategoryById);
+// router.put('/:id', authenticate, categoryController.updateCategory);
+// router.delete('/:id', authenticate, categoryController.deleteCategory);
 
 module.exports = router;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,10 +5,10 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', authenticate, productController.getAllProducts);
-router.post('/', authenticate, upload.single('image'), productController.createProduct);
-router.get('/:id', authenticate, productController.getProductById);
-router.put('/:id', authenticate, upload.single('image'), productController.updateProduct);
-router.delete('/:id', authenticate, productController.deleteProduct);
+router.get('/', productController.getAllProducts);
+router.post('/', upload.single('image'), productController.createProduct);
+router.get('/:id', productController.getProductById);
+router.put('/:id', upload.single('image'), productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
