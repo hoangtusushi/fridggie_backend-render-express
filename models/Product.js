80058328b095908f4bbdd1a61667e7a50b0d2404
Hiepdv2003 <hiepdvgcs210944@fpt.edu.vn>
2024-09-19 09:55:57 +0700

    modified product model

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    barcode: { type: String },
     image: String,
     name: { type: String, required: true },
-    production_date: { type: Date, required: true },
-    expiration_date: { type: Date, required: true },
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     quantity: { type: Number, required: true },
     used_quantity: { type: Number, default: 0 },
